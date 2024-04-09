6bb3145ca5eaa96aa56b9fc2359fb42d093f410d
Akumbom <akumbom5ma@gmail.com>
2024-04-09 13:13:18 +0100

    initial commit

---
+++ svg-generator-api/Controllers/SVGGenerator.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace svg_generator_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SVGGenerator : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        public SVGGenerator(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet(Name ="GetFigureDimensions")]
+        public async Task<IActionResult> GetFigureDimension()
+        {
+            try
+            {
+                var dimensions = await GetDimensions();
+                return Ok(dimensions);
+
+            }
+            catch(Exception ex)
+            {
+                var msg = ex.Message;
+                throw;
+            }
+        }
+        private async Task<SVGFigureDimensions> GetDimensions()
+        {
+            using (StreamReader reader = new StreamReader(SVGFigureDimensions.GetFilePath))
+            {
+                var dimensions = JsonConvert.DeserializeObject<SVGFigureDimensions>(await reader.ReadToEndAsync());
+                return dimensions;
+            }
+        }
+        [HttpPost(Name = "SaveFigureDimensions")]
+        public async void GetFigureDimension([FromBody] SVGFigureDimensions body)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(body);
+                byte[] data = Encoding.UTF8.GetBytes(json);
+
+                using (FileStream fileStream = System.IO.File.Open(SVGFigureDimensions.GetFilePath, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    fileStream.Write(data, 0, data.Length);
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                var msg = ex.Message;
+            }
+        }
+    }
+}
---
+++ svg-generator-api/SVGFigureDimensions.cs
@@ -0,0 +1,10 @@
+﻿namespace svg_generator_api
+{
+    public class SVGFigureDimensions
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public static string GetFilePath => $"{Directory.GetCurrentDirectory()}\\svg.json";
+    }
+}
